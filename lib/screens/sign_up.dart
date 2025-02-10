63c6ac5c6875d8b9d7f9e1ad4950f1643387351d
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-02-10 06:35:40 +0530

    Fixed keyboard resize issue and improved responsiveness

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -8,7 +8,10 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -16,150 +19,153 @@ class SignUpPage extends StatelessWidget {
             colors: [Color(0xFFF4EEF2), Color(0xFFF4EEF2), Color(0xFFE3EDF5)],
           ),
         ),
+
         child: SafeArea(
-          child: ListView(
-            children: [
-              SizedBox(
-                height: size.height * 0.1,
-              ),
-              // logo
-              Image.asset(
-                "assets/images/logo-blue.png", //change the image
-                height: size.height *
-                    0.25, // Adjust size proportionally to screen height
-                fit: BoxFit.contain,
-              ),
-              // welcome text
-              SizedBox(
-                height: size.height * 0.02,
-              ),
-              Text(
-                "Welcome to WonderSri !\nYour personal e-tour guide.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black38,
-                    height: 1.2,
-                    fontWeight: FontWeight.w800),
-              ),
-              // email password input area
-              SizedBox(height: size.height * 0.03),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: Text(
-                  "Create your account",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 17,
-                      color: Colors.black),
-                ),
-              ),
-              myTextFields("User Name", "Enter a user name"),
-              myTextFields("Email", "Enter your email"),
-              myTextFields("Password", "Enter your password"),
-              myTextFields("Confirm Password", "Re-enter your password"),
-              SizedBox(height: size.height*0.01),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    // sign in Button
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigator.pushReplacement(
-                        //   context,
-                        //   MaterialPageRoute(builder: (context) => HomePage()),
-                        // );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        elevation: 10, // Adds a shadow
-                        backgroundColor: Color(0xFF2D46B9),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        minimumSize: const Size.fromHeight(50),
-                      ),
-                      child: const Text(
-                        "Sign up",
-                        style: TextStyle(fontSize: 22, color: Colors.white),
-                      ),
-                    ),
-                    SizedBox(height: size.height * 0.02),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+          child:LayoutBuilder(
+            builder: (context,constraints) {
+              return SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child:ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: constraints.maxHeight,
+                  ),
+                  child:Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+
+                    child:Column(
                       children: [
-                        Container(
-                          height: 2,
-                          width: size.width * 0.2,
-                          color: Colors.black45,
+                        SizedBox(height: size.height * 0.075),
+                        // logo
+                        Image.asset(
+                          "assets/images/logo-blue.png", //change the image
+                          height: size.height * 0.25, // Adjust size proportionally to screen height
+                          fit: BoxFit.contain,
                         ),
+
+                        SizedBox(height: size.height * 0.01),
+
+                        // welcome text
                         Text(
-                          "  Or Sign up with  ",
+                          "Welcome to WonderSri !\nYour personal e-tour guide.",
+                          textAlign: TextAlign.center,
                           style: TextStyle(
-                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.black38,
+                              height: 1.2,
+                              fontWeight: FontWeight.w800),
+                        ),
+
+                        SizedBox(height: size.height * 0.02),
+
+                        // email password input area
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Create your account",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 17,
+                                color: Colors.black),
+                          ),
+                        ),
+
+                        myTextFields("User Name", "Enter a user name"),
+                        myTextFields("Email", "Enter your email"),
+                        myTextFields("Password", "Enter your password"),
+                        myTextFields("Confirm Password", "Re-enter your password"),
+
+                        SizedBox(height: size.height * 0.01),
+
+                        // sign in Button
+                        ElevatedButton(
+                          onPressed: () {
+                            // Navigator.pushReplacement(
+                            //   context,
+                            //   MaterialPageRoute(builder: (context) => HomePage()),
+                            // );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 10, // Adds a shadow
+                            backgroundColor: Color(0xFF2D46B9),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            minimumSize: const Size.fromHeight(50),
+                          ),
+                          child: const Text(
+                            "Sign up",
+                            style: TextStyle(fontSize: 22, color: Colors.white),
+                          ),
+                        ),
+
+                        SizedBox(height: size.height * 0.02),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 2,
+                              width: size.width * 0.2,
                               color: Colors.black45,
-                              fontSize: 14),
+                            ),
+                            Text(
+                              "  Or Sign up with  ",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black45,
+                                  fontSize: 14),
+                            ),
+                            Container(
+                              height: 2,
+                              width: size.width * 0.2,
+                              color: Colors.black45,
+                            ),
+                          ],
                         ),
-                        Container(
-                          height: 2,
-                          width: size.width * 0.2,
-                          color: Colors.black45,
-                        ),
-                      ],
-                    ),
-                    // sign in options
-                    SizedBox(height: size.height * 0.02),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // Google icon and text in a Row
-                        Container(
-                          width: size.width - size.width * 0.1,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            border: Border.all(color: Colors.white, width: 2),
+
+                        SizedBox(height: size.height * 0.02),
+
+                        ElevatedButton(
+                          onPressed: (){
+                            // Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                            minimumSize: const Size.fromHeight(50)
                           ),
                           child: Row(
                             children: [
-                              Image.asset(
-                                "assets/images/google.png", // Google icon
-                                height: 33,
-                              ),
+                              Image.asset("assets/images/google.png",height: 33),
                               Spacer(),
-                              Text(
-                                "Sign up with Google", // text
+                              Text("Sign up with Google",
                                 style: TextStyle(
-                                  color: Colors.black45,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
+                                  color: Colors.black45,fontWeight: FontWeight.bold,fontSize: 18
                                 ),
                               ),
                               Spacer(),
                             ],
                           ),
+
                         ),
+
+                        SizedBox(height: size.height * 0.02),
                       ],
                     ),
-                    SizedBox(height: size.height * 0.02),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
+              );
+            }
+          )
         ),
       ),
     );
   }
 
   // container for email and password input fields
-  Container myTextFields(String label, String hint) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: 25,
-        vertical: 10,
-      ),
+  Widget myTextFields(String label, String hint) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5),
       child: TextField(
         decoration: InputDecoration(
           contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
