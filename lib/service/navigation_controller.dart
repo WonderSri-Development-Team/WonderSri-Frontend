c7bb516d5322266f1392a085cac95697fbce282b
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-02-11 12:33:46 +0530

    linked all the main pages to the nav bar

--- lib/service/navigation_controller.dart
+++ lib/service/navigation_controller.dart
@@ -4,6 +4,7 @@ import 'package:frontend/screens/nav_bar.dart';
 import 'package:frontend/screens/home_page.dart';
 import 'package:frontend/screens/user_profile/SettingPage.dart';
 import 'package:frontend/screens/map/map_screen.dart';
+import 'package:frontend/screens/explorer/explorer_page.dart';
 
 class NavController extends StatefulWidget{
   const NavController({super.key});
@@ -18,7 +19,7 @@ class _NavControllerState extends State<NavController>{
   final List<Widget> _screens = [
     HomePage(),
     MapScreen(),
-    ExplorerPage(),
+    ExplorerScreen(),
     SettingsPage(),
 
 
