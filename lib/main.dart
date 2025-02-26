32b43dccb021ba7cb06ee1926339377815bd6371
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-02-26 09:41:50 +0530

    IMplemented Google maps and indicates user's live location on it

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import './screens/sign_in.dart';
 
-void main() {
+void main() async{
+  await dotenv.load(fileName: ".env");
+  final String mapsApiKey = dotenv.get('MAPS_API_KEY');
   runApp(const MyApp());
 }
 
