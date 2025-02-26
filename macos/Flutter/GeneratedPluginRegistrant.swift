29614540163ee565eea34b703f4fa4e4e0878845
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-02-26 09:54:20 +0530

    Solved some dependency conflicts and secured API keys

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import geolocator_apple
-import url_launcher_macos
 import google_sign_in_ios
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
