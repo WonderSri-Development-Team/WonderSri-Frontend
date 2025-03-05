6ff1bae62ca7407e41313b5342c84618794c08aa
Chamath Jayashanka <160167403+chamathjay@users.noreply.github.com>
2025-03-05 12:25:34 +0530

    Update .gitignore to include Android build artifacts

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:frontend/screens/home_page.dart';
+import 'package:frontend/screens/sign_in.dart';
 import 'package:frontend/screens/test_screen.dart';
 import 'package:frontend/service/firebase/firebase_notification.dart';
-import './screens/sign_in.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // load environment variables
   await dotenv.load();
-
-  _initializeFirebase();
+  // initialize firebase
+  await _initializeFirebase();
 
   runApp(const MyApp());
 }
@@ -19,14 +20,21 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: '/',
-      routes: {
-        '/': (context) => const LoginPage(),
-        // '/test': (context) => TestScreen(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (context) => const LoginPage());
+          case '/home':
+            return MaterialPageRoute(builder: (context) => const HomePage());
+          case '/test':
+            return MaterialPageRoute(builder: (context) => const TestScreen());
+          default:
+            return null;
+        }
       },
     );
   }
@@ -34,17 +42,19 @@ class MyApp extends StatelessWidget {
 
 Future<void> _initializeFirebase() async {
   try {
+    print("Initializing Firebase...");
     await Firebase.initializeApp(
       name: "WonderSri",
       options: FirebaseOptions(
-        apiKey: dotenv.env['FIREBASE_API_KEY']!,
-        appId: dotenv.env['FIREBASE_APP_ID']!,
-        projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+        apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_APP_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
       ),
     );
+    print("Firebase initialized!");
     FirebaseNotification().initNotification();
   } catch (e) {
-    print("Firebase initialization failed: $e");
+    debugPrint("Firebase initialization failed: $e");
   }
 }
