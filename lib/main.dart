9d7393e624d2f2402466e0eedd57f84a6553a217
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-03-15 04:05:27 +0530

    Implemented the UI for Foods , events and activities and backend connected to fetch data according to the user's location

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import './screens/sign_in.dart';
+import 'package:provider/provider.dart';
+import './service/location_provider.dart';
+
 
 void main() async{
   await dotenv.load(fileName: ".env");
   final String mapsApiKey = dotenv.get('MAPS_API_KEY');
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LocationProvider()), // Add LocationProvider
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
