1c1995fbeec2e51acf7f513bff5b8e4c20226d49
Chamath Jayashanka <160167403+chamathjay@users.noreply.github.com>
2025-03-23 12:48:12 +0530

    Remove unused import

--- lib/screens/notifications_page.dart
+++ lib/screens/notifications_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
-import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 import 'package:timeago/timeago.dart' as timeago;
 
